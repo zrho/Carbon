ae3df44fead52f99d78f83e8e769500a42182551
Lukas Heidemann <lukasheidemann@aim.com>
2011-11-06 21:48:50 +0100

    libc/carbon now uses futexes for mutex support to prepare for deprecation of mutex_* syscalls.

--- libc/src/carbon/mutex/mutex_unlock.c
+++ libc/src/carbon/mutex/mutex_unlock.c
@@ -16,39 +16,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#pragma once
-#include <stdbool.h>
+#include <carbon/mutex.h>
+#include <carbon/futex.h>
 
-//- API - Multitasking - Synchronization ---------------------------------------
-
-/**
- * Type for mutexes.
- */
-typedef futex_t mutex_t;
-
-/**
- * Initial value for (unlocked) mutexes.
- */
-#define MUTEX_INIT 0
-
-/**
- * Waits until the mutex has been unlocked and locks it.
- *
- * @param mutex The mutex to be locked.
- */
-void mutex_lock(mutex_t *mutex);
-
-/**
- * Tries to acquire the mutex, but does not wait if it is already locked.
- *
- * @param mutex The mutex to be locked.
- * @return Whether the mutex has been locked.
- */
-bool mutex_trylock(mutex_t *mutex);
-
-/**
- * Unlocks the mutex.
- *
- * @param mutex To mutex to be unlocked.
- */
-void mutex_unlock(mutex_t *mutex);
+void mutex_unlock(mutex_t *mutex) {
+    *mutex = 0;
+    futex_wake((futex_t *) mutex, 0, 1);
+}
